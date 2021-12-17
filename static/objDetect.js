eeef2f53253b1b0ef42e2859ea3a2ad7bd322374
darshil3011 <45987056+darshil3011@users.noreply.github.com>
2021-12-17 16:07:52 +0530

    added comments

--- static/objDetect.js
+++ static/objDetect.js
@@ -1,9 +1,3 @@
-/**
- * Created by chad hart on 11/30/17.
- * Client side of Tensor Flow Object Detection Web API
- * Written for webrtcHacks - https://webrtchacks.com
- */
-
 //Parameters
 const s = document.getElementById('objDetect');
 const sourceVideo = s.getAttribute("data-source");  //the source video to use
